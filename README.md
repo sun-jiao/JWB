36dc84a6b9d6735fae5d8fb9141f94aebc076f9d
Sun Jiao <P.ozzy@outlook.com>
2023-08-05 08:39:27 +0800

    update and reformat

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-This repository contains the files required to run the JavaScript Wiki Browser developed by Joeytje50. The live version that can be used for importing into your wiki can be found at [English Wikipedia (User:Joeytje50/JWB)](http://en.wikipedia.org/wiki/User:Joeytje50/JWB).
+This repository contains the files required to run the JavaScript Wiki Browser developed by Joeytje50. The live version
+that can be used for importing into your wiki can be found
+at [English Wikipedia (User:Joeytje50/JWB)](http://en.wikipedia.org/wiki/User:Joeytje50/JWB).
 
-Because of a different structure of files on Wikipedia, here's an overview of which Wikipedia page a certain file corresponds to:
+Because of a different structure of files on Wikipedia, here's an overview of which Wikipedia page a certain file
+corresponds to:
 
 | (Git file)           | ENWP/User:Joeytje50/$1                                                                             |
 |----------------------|----------------------------------------------------------------------------------------------------|
@@ -16,8 +19,16 @@ Because of a different structure of files on Wikipedia, here's an overview of wh
 | RETF/README.wikitext | [RETF](http://en.wikipedia.org/wiki/User:Joeytje50/RETF)                                           |
 | README.md            | *N/A*                                                                                              |
 
-Author attribution can be found in the **JWB.js** file, and for the i18n contributions, authors are listed in the respective **i18n/XX.js** file for the language(s) they translated. For a comprehensive file history of each of the files starting at version 2.0, see [the history of the JWB.js page](https://en.wikipedia.org/w/index.php?title=User:Joeytje50/JWB.js&action=history) on the English Wikipedia. For version 1.0, see [the original version history on the RuneScape Wiki](https://runescape.wiki/w/User:Joeytje50/AWB.js?action=history).
+Author attribution can be found in the **JWB.js** file, and for the i18n contributions, authors are listed in the
+respective **i18n/XX.js** file for the language(s) they translated. For a comprehensive file history of each of the
+files starting at version 2.0,
+see [the history of the JWB.js page](https://en.wikipedia.org/w/index.php?title=User:Joeytje50/JWB.js&action=history) on
+the English Wikipedia. For version 1.0,
+see [the original version history on the RuneScape Wiki](https://runescape.wiki/w/User:Joeytje50/AWB.js?action=history).
 
-If you want to modify this script and test it, you may need to modify some of the hardcoded URLs in the scripts. These URLs are conveniently placed at the top of the JWB.js file. Please do revert the urls back to the hardcoded wikipedia URLs when making a pull request into the master branch, in order to have a working version on this branch at all time.
+If you want to modify this script and test it, you may need to modify some of the hardcoded URLs in the scripts. These
+URLs are conveniently placed at the top of the JWB.js file. Please do revert the urls back to the hardcoded wikipedia
+URLs when making a pull request into the master branch, in order to have a working version on this branch at all time.
 
-A change log for changes made after version 4.0.0 can be found on Wikipedia, at [User:Joeytje50/JWB/Changelog](https://en.wikipedia.org/wiki/User:Joeytje50/JWB/Changelog).
+A change log for changes made after version 4.0.0 can be found on Wikipedia,
+at [User:Joeytje50/JWB/Changelog](https://en.wikipedia.org/wiki/User:Joeytje50/JWB/Changelog).
--- RETF/RETF.js
+++ RETF/RETF.js
@@ -3,14 +3,14 @@
 /**<nowiki>
  * Usage: See [[User:Joeytje50/RETF]].
 
-importScript('User:Joeytje50/RETF.js'); //backlink: [[User:Joeytje50/RETF.js]]
-RETF.replace(text);
+ importScript('User:Joeytje50/RETF.js'); //backlink: [[User:Joeytje50/RETF.js]]
+ RETF.replace(text);
 
  * This will perform all of the replacement rules on the Project:AutoWikiBrowser/Typos page on wiki it's run from
  * on the entered piece of text via the first argument.
  * To reload the typo list, simply run the following:
 
-RETF.load();
+ RETF.load();
 
  * @licence
  * This program is free software; you can redistribute it and/or modify
@@ -34,63 +34,63 @@ RETF.load();
 window.RETF = {}; //making a main object to store everything else
 RETF.list = [];
 
-RETF.buildList = function(response) {
-	if (response.query && response.query.pageids[0] === '-1') return; //abort if typos page doesn't exist.
+RETF.buildList = function (response) {
+    if (response.query && response.query.pageids[0] === '-1') return; //abort if typos page doesn't exist.
     var page = response.query.pages[response.query.pageids[0]];
     var cont = page.revisions[0]['*'];
-	//make sure the string starts with a < by putting <typos></typos> around.
-	var $typolists = $('<typos>'+cont+'</typos>').find('Typo:not([disabled])').each(function() {
-		if ($(this).attr('src') !== undefined) {
-			// Allow 'importing' of external typo fixing rules. Format for the URL needs to be exactly like:
-			// https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvprop=content&rvlimit=1&indexpageids=true&origin=*&titles=Project%3AAutoWikiBrowser%2FTypos
-			// (changes to the base API url and changes to the page name being allowed); origin=* is important to allow CORS requests.
-			$.get($(this).attr('src'), RETF.buildList);
-		} else {
-			try {
-				new RegExp($(this).attr('find')); // will throw a SyntaxError in browsers not supporting lookbehinds
-				RETF.list.push([$(this).attr('find'), $(this).attr('replace'), $(this).attr('word')]); //store all typofixes in one big array
-			} catch(e) {
-				// skip rule if it contains lookbehinds.
-			}
-		}
-	});
+    //make sure the string starts with a < by putting <typos></typos> around.
+    var $typolists = $('<typos>' + cont + '</typos>').find('Typo:not([disabled])').each(function () {
+        if ($(this).attr('src') !== undefined) {
+            // Allow 'importing' of external typo fixing rules. Format for the URL needs to be exactly like:
+            // https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&rvprop=content&rvlimit=1&indexpageids=true&origin=*&titles=Project%3AAutoWikiBrowser%2FTypos
+            // (changes to the base API url and changes to the page name being allowed); origin=* is important to allow CORS requests.
+            $.get($(this).attr('src'), RETF.buildList);
+        } else {
+            try {
+                new RegExp($(this).attr('find')); // will throw a SyntaxError in browsers not supporting lookbehinds
+                RETF.list.push([$(this).attr('find'), $(this).attr('replace'), $(this).attr('word')]); //store all typofixes in one big array
+            } catch (e) {
+                // skip rule if it contains lookbehinds.
+            }
+        }
+    });
 };
 
-RETF.load = function() {
-	RETF.list = []; //reset list, in case this is a list refresh.
-	(new mw.Api()).get({
-		action: 'query',
-		prop: 'revisions',
+RETF.load = function () {
+    RETF.list = []; //reset list, in case this is a list refresh.
+    (new mw.Api()).get({
+        action: 'query',
+        prop: 'revisions',
         titles: 'Project:AutoWikiBrowser/Typos',
         rvprop: 'content',
         rvlimit: '1',
         indexpageids: true,
-		format: 'json',
-	}).done(RETF.buildList);
+        format: 'json',
+    }).done(RETF.buildList);
 };
 
-RETF.replace = function(text) {
-	var rulesList = [];
-	var replaceText = function(match, g1) {
-		//don't perform replaces when one of the excluded things has been matched (indicated by group 1 not being matched).
-		if (!g1) return match;
-		rulesList.push(RETF.list[i][2]);
-		return g1.replace(new RegExp(RETF.list[i][0], 'g'), RETF.list[i][1]);
-	};
-	for (var i=0;i<RETF.list.length;i++) {
-		//checks if the rule matches any wikilink targets.
-		var wikiLinkTarget = new RegExp('\\[\\[[^\\|\\]]*' + RETF.list[i][0] + '[^\\|\\]]*(\\||\\]\\])');
-		if (wikiLinkTarget.test(text)) {
-			continue; //ignore this rule
-		}
-		//Regex based on http://stackoverflow.com/a/23589204/1256925.
-		//Rules to skip based on https://en.wikipedia.org/wiki/Wikipedia:AutoWikiBrowser/Typos#AutoWikiBrowser_.28AWB.29
-		//Rules are: File:links | templates | "quotes" | text after : or * 
-		var exclude = new RegExp('\\[\\[File:[^\\|\\]]+\\|?|{{.+?}}|"[^"]+"|[:\\*].*|('+RETF.list[i][0]+')', 'ig');
-		text = text.replace(exclude, replaceText);
-	}
-	console.log('RegEx Typo Fixing: Found rules ' + rulesList.join(', '));
-	return text;
+RETF.replace = function (text) {
+    var rulesList = [];
+    var replaceText = function (match, g1) {
+        //don't perform replaces when one of the excluded things has been matched (indicated by group 1 not being matched).
+        if (!g1) return match;
+        rulesList.push(RETF.list[i][2]);
+        return g1.replace(new RegExp(RETF.list[i][0], 'g'), RETF.list[i][1]);
+    };
+    for (var i = 0; i < RETF.list.length; i++) {
+        //checks if the rule matches any wikilink targets.
+        var wikiLinkTarget = new RegExp('\\[\\[[^\\|\\]]*' + RETF.list[i][0] + '[^\\|\\]]*(\\||\\]\\])');
+        if (wikiLinkTarget.test(text)) {
+            continue; //ignore this rule
+        }
+        //Regex based on http://stackoverflow.com/a/23589204/1256925.
+        //Rules to skip based on https://en.wikipedia.org/wiki/Wikipedia:AutoWikiBrowser/Typos#AutoWikiBrowser_.28AWB.29
+        //Rules are: File:links | templates | "quotes" | text after : or *
+        var exclude = new RegExp('\\[\\[File:[^\\|\\]]+\\|?|{{.+?}}|"[^"]+"|[:\\*].*|(' + RETF.list[i][0] + ')', 'ig');
+        text = text.replace(exclude, replaceText);
+    }
+    console.log('RegEx Typo Fixing: Found rules ' + rulesList.join(', '));
+    return text;
 };
 
 RETF.load();
